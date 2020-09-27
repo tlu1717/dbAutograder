87d3def8048af193377cd96bf1089b767f6113d0
tlu17 <tlu17@illinois.edu>
2020-09-26 21:07:45 -0500

    order the files alphabetically in folder

--- gradescript.sh
+++ gradescript.sh
@@ -8,18 +8,18 @@ function combine_files {
 	for studentFolder in $hwFolder/*
 	do
 		local NAMEPATH=$(cut -d"/" -f 2 <<< $studentFolder)
-		for f in $studentFolder/*.sql
+		for f in `ls $studentFolder/*.sql | sort -g`
 		do
 			#run sql files individually in case of missing semi colons
 			(cat $f | mysql -u root -p$PASSWORD -t) > ${f%???}txt
 
 			#concat queries 
-			cat $f  >> ./CH3-queries/${NAMEPATH}_query.txt
-			sed -i '' -e '$a\' ./CH3-queries/${NAMEPATH}_query.sql
+			#cat $f  >> ./${NAMEPATH}_query.txt
+			#sed -i '' -e '$a\' ./${NAMEPATH}_query.sql
 			
 		done
 		echo $studentFolder
-		cat $studentFolder/*.txt >> ./CH3-Results/${NAMEPATH}_output.txt
+		cat $studentFolder/*.txt >> ./${NAMEPATH}_output.txt
 	done
 }
 
